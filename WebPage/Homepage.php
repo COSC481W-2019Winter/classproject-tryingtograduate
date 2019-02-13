86b1548d577820caf112bb33002d0bf8e2659bb8
bfoley693 <bfoley693@gmail.com>
2019-02-12 23:43:39 -0500

    Update Homepage.php

--- WebPage/Homepage.php
+++ WebPage/Homepage.php
@@ -63,8 +63,11 @@
 	$fName = $_POST['fname'];
 	$lName = $_POST['lname'];
 	$eMail = $_POST['email'];
-	$password = $_POST['passwordNew'];
+	$passwordNew = $_POST['passwordNew'];
 	$phoneNum = $_POST['phone'];
+	
+	$username = $password = "";
+	$username_err = $password_err = "";
 	
 	// Create connection
 	$conn = new mysqli($servername, $db_username, $db_password, $db_name);
@@ -78,6 +81,73 @@
 	{
 		echo "Connection Successful!!!!!";
 		//output variables to make sure they are being stored properly
+	}
+	
+	if(isset($_POST['SignIn'])){
+		// Check if username is empty
+		if(empty(trim($_POST["username"]))){
+			$username_err = "Please enter username.";
+		}else{
+			$username = trim($_POST["username"]);
+		}
+    
+    // Check if password is empty
+		if(empty(trim($_POST["password"]))){
+			$password_err = "Please enter your password.";
+		}else{
+			$password = trim($_POST["password"]);
+		}
+		
+		// Validate credentials
+		if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+			$sql = "SELECT id, emailAddress, password FROM Person WHERE emailAddress = '$username'";
+        
+			if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+				mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+				$param_username = $username;
+            
+            // Attempt to execute the prepared statement
+				if(mysqli_stmt_execute($stmt)){
+                // Store result
+					mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+					if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+						mysqli_stmt_bind_result($stmt, $id, $username, $passwordHash);
+						if(mysqli_stmt_fetch($stmt)){
+							if(password_verify($password, $passwordHash)){
+                            // Password is correct, so start a new session
+								session_start();
+                            
+                            // Store data in session variables
+								$_SESSION["loggedin"] = true;
+								$_SESSION["id"] = $id;
+								$_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+								header("location: welcome.php");
+							}else{
+                            // Display an error message if password is not valid
+								$password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
 	}
 	
 	//Testing to see if the submit button has been pressed referenced by name
@@ -96,7 +166,7 @@
 		{
 			//Inserts new record into table from sql statement
 			mysqli_query($conn, "INSERT INTO Person(firstName, lastName, emailAddress, passwordHash, phoneNumber) 
-				VALUES ('$fName', '$lName', '$eMail', '$password', '$phoneNum')");
+				VALUES ('$fName', '$lName', '$eMail', '$passwordNew	', '$phoneNum')");
 			
 			//Check the status of the query
 			if (mysqli_affected_rows($conn) > 0)
