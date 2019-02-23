208817c8ae852d110ab75a86a58565b67e280fd4
Tyler Boyd <46505879+tboyd10@users.noreply.github.com>
2019-02-23 14:24:25 -0500

    Fixed Select Groups Statement.
    Fixed broken SQL Statements.

--- WebPage/groups/index.php
+++ WebPage/groups/index.php
@@ -25,7 +25,6 @@
         <div id="all">
 						<select id="glist">
 							<option value="0">Select Group</option>
-
 							<?php
 								//Variables created to access the database on Wi2017_436_kbledsoe3
 							  $servername = "localhost";
@@ -35,20 +34,19 @@
 
 								//Variables created to reference input textboxes, reference html by name
 								//SignUp Variables
-								$UserId = 123456;
-
+								$UserId = 13;
 
 								// Create connection
 								$conn = new mysqli($servername, $db_username, $db_password, $db_name);
 
 								$sql = "SELECT groupId, groupName FROM Groups WHERE ownerId = $UserId";
 								$result = $conn->query($sql);
-
 								if ($result->num_rows > 0) {
 								    // output data of each row
 								    while($row = $result->fetch_assoc()) {
-								        echo "<option value=\"" .$row["groupId"]. "\">".$row[groupName]"</option>";
-								    }
+								       echo "<option value=\"". $row["groupId"]. "\">". $row["groupName"]."</option>";
+
+							    	}
 								} else {
 								    echo "<option value=\"0\">You have no groups</option>";
 								}
@@ -57,15 +55,16 @@
 								$conn->close();
 							?>
 
+
 						</select>
             <button class="button" id="addGroup" onclick="newGroupFunc()">Add Group</button>
             <button class="button" id="editGroup">Edit Group</button>
             <button class="button" id="exit" onclick = "window.location.href = '../dashboard'">Exit to Main</button>
 
 						<div id="newGroup">
-							<form class="newGroup">
+							<form class="newGroup" action = "" method = "post">
 								<label>Group name:</label>
-								<input type="text" id="newGName" placeholder="Group Name">
+								<input type="text" name="newGName" placeholder="Group Name">
 								<input class="button" id="addGbutton" type="submit" name="addGtoDB" value="ADD" onclick="newGroupFunc">
 								<button id="cancel" onclick="newGroupFunc">CANCEL</button>
 							</form>
@@ -74,9 +73,9 @@
 						<div id="contactlist">
 							<table>
 								<tr>
-									<thead>Name</thead>
-									<thead>Phone</thead>
-									<thead>E-Mail</thead>
+									<thead></thead>
+									<thead></thead>
+									<thead></thead>
 								</tr>
 							</table>
 						</div>
@@ -85,6 +84,8 @@
         </div>
 	</body>
 </html>
+
+<!-- CHANGE 5.0 -->
 <?php
 	//Variables created to access the database on Wi2017_436_kbledsoe3
   $servername = "localhost";
@@ -95,21 +96,13 @@
 	//Variables created to reference input textboxes, reference html by name
 	//SignUp Variables
 	$newGroupName = $_POST['newGName'];
+  $UserId = 13;
 
 
 	// Create connection
 	$conn = new mysqli($servername, $db_username, $db_password, $db_name);
 	// Check connection
-	if ($conn->connect_error)
-	{
-    	echo "could not establish connection to db2...";
-		die("Connection failed: " . $conn->connect_error);
-	}
-	else
-	{
-		echo "Connection Successful!!!!!";
-		//output variables to make sure they are being stored properly
-	}
+
 
 	//Testing to see if the SignUp button has been pressed referenced by html name
 	if(isset($_POST['addGtoDB']))
@@ -126,13 +119,17 @@
 		else //if email does not exist
 		{
 			//Inserts new record into table from sql statement
-			mysqli_query($conn, "INSERT INTO Groups(groupName) VALUES ('$newGroupName')");
+			mysqli_query($conn, "INSERT INTO Groups(groupName, ownerId) VALUES ('$newGroupName','$UserId')");
 
 			//Check the status of the query
 			if (mysqli_affected_rows($conn) > 0)
 			{
 				echo '<script language="javascript">';
 				echo 'alert("Added successfully!!")';
+				echo '</script>';
+				// Re-route
+				echo '<script language="javascript">';
+				echo 'window.location.href ="../groups/"' ;
 				echo '</script>';
 			}
 			else
