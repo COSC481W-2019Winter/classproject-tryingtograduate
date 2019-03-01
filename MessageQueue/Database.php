530449113e11e5fc767845025dc2834c2f16a493
Josh Peck <jpek3@emich.edu>
2019-03-01 07:08:39 +0000

    reconfig database connection to align with db2 and devlamp

--- MessageQueue/Database.php
+++ MessageQueue/Database.php
@@ -1,6 +1,6 @@
 <?php
   const SERVERNAME = "localhost";
-  const USERNAME = "cpuser";
-  const PASSWORD = "1234";
-  const DBNAME = "carrier_pidgeon";
+  const USERNAME = "kbledsoe3";
+  const PASSWORD = "1784793b4a";
+  const DBNAME = "Wi2017_436_kbledsoe3";
 ?>
