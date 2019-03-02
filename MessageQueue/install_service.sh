65dfbf3140d5ede409571eebd8a6daa23067a540
Josh Peck <jpeck3@emich.edu>
2019-03-02 01:40:50 -0500

    moved service install script

--- MessageQueue/install_service.sh
+++ MessageQueue/install_service.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo mkdir /opt/CarrierPigeon
-sudo ln -s ../MessageQueue /opt/CarrierPigeon/MessageQueue
+sudo ln -s MessageQueue /opt/CarrierPigeon/MessageQueue
 sudo ln -s /opt/CarrierPigeon/MessageQueue/cpigeon.service /etc/systemd/system/cpigeon.service
 sudo adduser cpigeon
+sudo systemctl daemon-reload 
 sudo systemctl enable cpigeon
 sudo systemctl start cpigeon
