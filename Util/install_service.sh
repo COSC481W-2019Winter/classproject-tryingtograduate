aa1898a6344db386e0c1b9a5641236a77da41541
Josh Peck <jpeck3@emich.edu>
2019-03-02 01:24:39 -0500

    MessageQueue service installation script

---
+++ Util/install_service.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo mkdir /opt/CarrierPigeon
+sudo ln -s ../MessageQueue /opt/CarrierPigeon/MessageQueue
+sudo ln -s /opt/CarrierPigeon/MessageQueue/cpigeon.service /etc/systemd/system/cpigeon.service
+sudo systemctl enable cpigeon
+sudo systemctl start cpigeon
