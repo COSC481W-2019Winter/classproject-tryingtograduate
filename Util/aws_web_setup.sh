0ecd3e08092414c0cebeb9efb26c32d5141f1499
Josh Peck <jpeck3@emich.edu>
2019-03-03 15:46:17 -0500

    updated aws web setup script to canfigure database constants when front end refactor is complete

--- Util/aws_web_setup.sh
+++ Util/aws_web_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+CURDIR=`pwd`
 cd /var/www/html
 sudo rm -rf *
 sudo cp -r /home/ec2-user/classproject-tryingtograduate/WebPage/* .
+cd /var/www/html/PHP
+rm -f Database.PHP
+mv AWS.php Database.php
+cd $CURDIR
