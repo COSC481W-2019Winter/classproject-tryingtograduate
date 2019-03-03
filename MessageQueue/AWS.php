80b2b138f19d34c55037aa9801a490d32d94e6f3
Josh Peck <jpeck3@emich.edu>
2019-03-03 14:44:14 -0500

    fixing typo in AWS connection steps.txt and adding AWS MessageQueue db config file

---
+++ MessageQueue/AWS.php
@@ -0,0 +1,6 @@
+<?php
+  const SERVERNAME = "wi2017436kbledsoe3.cjxnup8stayy.us-east-2.rds.amazonaws.com";
+  const USERNAME = "kbledsoe3";
+  const PASSWORD = "1784793b4a";
+  const DBNAME = "Wi2017436kbledsoe3";
+?>
