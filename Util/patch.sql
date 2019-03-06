aeadc9a8a9b7cf0d786f262b0b4cb354a8e080cd
Josh Peck <jpek3@emich.edu>
2019-03-06 21:27:10 +0000

    schema patch for database

--- Util/patch.sql
+++ Util/patch.sql
@@ -6,3 +6,7 @@ ADD FOREIGN KEY IF NOT EXISTS (`contactId`)
 ALTER TABLE `Message` 
 CHANGE COLUMN `lastSent` `lastSent` DATETIME NULL;
 
+ALTER TABLE `Message` 
+DROP FOREIGN KEY `Message_ibfk_2`;
+ALTER TABLE `Message` 
+DROP INDEX `groupId` ;
