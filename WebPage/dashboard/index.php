f34687c79196f9bdd2b9e29662903924f290e2e3
Kristal <kbledsoe3@emich.edu>
2019-03-06 11:14:28 -0500

    populated dropdowns on message dash

--- WebPage/dashboard/index.php
+++ WebPage/dashboard/index.php
@@ -6,6 +6,17 @@
 		<title>Carrier Pigeon</title>
 		<link rel="stylesheet" type="text/css" href="../CSS/messageStyle.css">
 		<script>
+			//function to grab the value of the "Username:" field on the Sign-In form of homepage
+			//method is called by "onload" in <body> tag---not currently being called (using $_SESSION)
+			function user(){
+
+				var user = "Not Working"
+				//loads the value into a variable called user
+				//use this value in sql query: SELECT uniqueId FROM Person where emailAddress = value in user var
+				user = localStorage.getItem("userId");
+				//echo's value to screen with message stating who is logged in
+				document.getElementById("user").innerHTML = "Logged in as:  " + user;
+			}
 			function saveMessage() {
 				var header = document.getElementById("saveMessage");
 				if (header.style.display === "none") {
@@ -26,12 +37,44 @@
 		<div id = "wrapper">
 			<div class="navbar">
 				<div class="dropdown">
-					<button class="dropbtn">Select Message </button>
-					<div class="dropdown-content">
-						<a href="#">Office Closed</a>
-						<a href="#">Staff Meeting</a>
-						<a href="#">Weather Alert</a>
-					</div>
+					<form name="glist" action="" method="post">
+						<select id="tmpList" name="tmpList" class = "dropbtn" >
+							<option  value="0"class="dropdown-content">Select Message</option>
+							<?php
+								session_start();
+								//Variables created to access the database on Wi2017_436_kbledsoe3
+							  $servername = "localhost";
+								$db_username = "kbledsoe3";     //Username for MySQL
+								$db_password = "1784793b4a";     //Password for MySQL
+								$db_name   = "Wi2017_436_kbledsoe3"; //Database name
+
+								//Variables created to reference input textboxes, reference html by name
+								//SignUp Variables
+								$UserEmail = $_SESSION['currentUserEmail'];
+
+
+								// Create connection
+								$conn = new mysqli($servername, $db_username, $db_password, $db_name);
+								$sql = "SELECT uniqueId FROM Person WHERE emailAddress = '$UserEmail' limit 1";
+								$result = $conn->query($sql);
+								$id = mysqli_fetch_object($result);
+								$UserId = $id->uniqueId;
+
+
+								$sql = "SELECT messageId, templateName FROM Message WHERE ownerId = $UserId";
+								$result = $conn->query($sql);
+								if ($result->num_rows > 0) {
+								    // output data of each row
+								    while($row = $result->fetch_assoc()) {
+								       echo "<option value=\"". $row["messageId"]. "\">". $row["templateName"]."</option>";
+							    	}
+								} else {
+								    echo "<option value=\"4\">You have no templates</option>";
+								}
+								$conn->close();
+							?>
+						</select>
+					</form>
 				</div>
 			</div>
 			<div class="navbar">
@@ -39,13 +82,49 @@
 			</div>
 			<div class="navbar">
 				<div class="dropdown">
-					<button class="dropbtn">Select Group</button>
-					<div class="dropdown-content">
-						<a href="#">Managers</a>
-						<a href="#">Contractors</a>
-						<a href="#">All Staff</a>
-					</div>
-				</div>
+
+					<form name="glist" action="" method="post">
+						<select id="glist" name="glist" class = "dropbtn">
+							<option value="0"class="dropdown-content">Select Group</option>
+							<?php
+								session_start();
+								//Variables created to access the database on Wi2017_436_kbledsoe3
+							  $servername = "localhost";
+								$db_username = "kbledsoe3";     //Username for MySQL
+								$db_password = "1784793b4a";     //Password for MySQL
+								$db_name   = "Wi2017_436_kbledsoe3"; //Database name
+
+								//Variables created to reference input textboxes, reference html by name
+								//SignUp Variables
+								$UserEmail = $_SESSION['currentUserEmail'];
+
+
+								// Create connection
+								$conn = new mysqli($servername, $db_username, $db_password, $db_name);
+								$sql = "SELECT uniqueId FROM Person WHERE emailAddress = '$UserEmail' limit 1";
+								$result = $conn->query($sql);
+								$id = mysqli_fetch_object($result);
+								$UserId = $id->uniqueId;
+
+
+								$sql = "SELECT groupId, groupName FROM Groups WHERE ownerId = $UserId";
+								$result = $conn->query($sql);
+								if ($result->num_rows > 0) {
+								    // output data of each row
+								    while($row = $result->fetch_assoc()) {
+								       echo "<option value=\"". $row["groupId"]. "\">". $row["groupName"]."</option>";
+
+							    	}
+								} else {
+								    echo "<option value=\"4\">You have no groups</option>";
+								}
+
+
+								$conn->close();
+							?>
+						</select>
+					</form>
+
 			</div>
 		</div>
 		</br>
@@ -58,13 +137,15 @@
 				</br></br>
 				<input id="addMsgButton" name = "addMsgButton" type="submit" value="SAVE">
 				<button id = "cancel">CANCEL</button>
-			</form>
+
 		</div>
 		</br></br>
 		<textarea id = "message" class = "center" name = "message">Type message here...</textarea>
 		</br></br>
+		</form>
 		<table class = "center">
 		<td>
+
 			<tr><td><button class = "button buttonA" onclick = "window.location.href ='../dashboard/index.php'" >
 			Send</button></td>
 			<td><button class = "button buttonB" onclick = "window.location.href ='../home/index.html'" >
@@ -80,7 +161,7 @@
 	//Variables needed to save the message
 	$tempName = $_POST['tempName'];
 	$tempSubject = $_POST['tempSubject'];
-	$tempMsg = $_GET['message'];
+	$tempMsg = $_POST['message'];
 	$groupId = '10';
 	$UserEmail = $_SESSION['currentUserEmail'];
 
@@ -104,29 +185,29 @@
 		$resultA = $conn->query($queryA);
 		$idA = mysqli_fetch_object($resultA);
 		$UniqueId = $idA->uniqueId;
-	
+
 		$queryB = "SELECT ownerId FROM Person WHERE emailAddress = '$UserEmail'";
 		$resultB = $conn->query($queryB);
 		$idB = mysqli_fetch_object($resultB);
 		$OwnerId = $idB->ownerId;
-		
+
 		$queryC = "SELECT firstName FROM Person WHERE emailAddress = '$UserEmail'";
 		$resultC = $conn->query($queryC);
 		$idC = mysqli_fetch_object($resultC);
-		$FirstNm = $idC->firstName;	
-	
+		$FirstNm = $idC->firstName;
+
 		$queryD = "SELECT lastName FROM Person WHERE emailAddress = '$UserEmail'";
 		$resultD = $conn->query($queryD);
 		$idD = mysqli_fetch_object($resultD);
-		$LastNm = $idD->lastName;	
+		$LastNm = $idD->lastName;
 
 		echo "Logged in as:  ", $FirstNm, " ", $LastNm;
 	}
-	//check to see if SAVE button has been clicked		
+	//check to see if SAVE button has been clicked
 	if(isset($_POST['addMsgButton']))
 	{
 		//Test to see if the template information entered already exists in the table
-		$query2 = mysqli_query($conn, "SELECT * FROM Message WHERE templateName = '$tempName' AND subject = '$tempSubject' AND ownerId = '$UserId';");
+		$query2 = mysqli_query($conn, "SELECT * FROM Message WHERE templateName = '$tempName' AND subject = '$tempSubject' AND ownerId = '$OwnerId';");
 
 		if ($query2->num_rows != 0) //if username exists
 		{
@@ -138,7 +219,7 @@
 		{
 			//Inserts new record into table from sql statement
 			mysqli_query($conn, "INSERT INTO Message(ownerId, groupId, subject, content, templateName)
-				VALUES ('$ownerId','$groupId', '$tempSubject', '$tempMsg', '$tempName')");
+				VALUES ('$UniqueId','$groupId', '$tempSubject', '$tempMsg', '$tempName')");
 
 			//Check the status of the query
 			if (mysqli_affected_rows($conn) > 0)
@@ -158,6 +239,6 @@
 				echo '</script>';
 			}
 		}
-	}		
+	}
 	$conn->close();
 ?>
