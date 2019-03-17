0fb320ea67f3096fad5b0b909fe1d9e81721ddc1
Josh Peck <jpeck3@emich.edu>
2019-03-17 09:32:42 -0400

    fixes for aws_web_setup.sh

--- Util/aws_web_setup.sh
+++ Util/aws_web_setup.sh
@@ -3,6 +3,7 @@ CURDIR=`pwd`
 cd /var/www/html
 sudo rm -rf *
 sudo cp -r /home/ec2-user/classproject-tryingtograduate/WebPage/* .
+sudo cp /home/ec2-user/classproject-tryingtograduate/Util/aws.html ./index.html
 cd /var/www/html/PHP
 sudo rm -f Database.PHP
 sudo mv AWS.php Database.php
