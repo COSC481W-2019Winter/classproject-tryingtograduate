d89771bb6323a01a4ca01f53d1c664d7a4d55862
Josh Peck <jpeck3@emich.edu>
2019-03-23 19:22:12 -0400

    sql patch for password hash column length complete 500 characters + SSL redirector for AWS instance

--- Util/patch.sql
+++ Util/patch.sql
@@ -1,5 +1,7 @@
 ALTER TABLE `Group_JT` 
-ADD COLUMN IF NOT EXISTS `contactId` INT(11) NOT NULL AFTER `groupId`, 
+ADD COLUMN IF NOT EXISTS `contactId` INT(11) NOT NULL AFTER `groupId`;
+
+ALTER TABLE `Group_JT` 
 ADD FOREIGN KEY IF NOT EXISTS (`contactId`)
   REFERENCES `Person` (`uniqueId`);
 
@@ -8,5 +10,9 @@ CHANGE COLUMN `lastSent` `lastSent` DATETIME NULL;
 
 ALTER TABLE `Message` 
 DROP FOREIGN KEY `Message_ibfk_2`;
+
 ALTER TABLE `Message` 
 DROP INDEX `groupId`;
+
+ALTER TABLE `Person` 
+CHANGE COLUMN `passwordHash` `passwordHash` VARCHAR(500) NULL DEFAULT NULL;
