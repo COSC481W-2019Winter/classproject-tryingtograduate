ad230cda4698f66d243efbca3fe08a79839b6991
hankerdm <46505876+hankerdm@users.noreply.github.com>
2019-03-26 18:39:26 -0400

    increased DB password hash field size

--- Database/mariadb/carrier_pigeon_ddl.sql
+++ Database/mariadb/carrier_pigeon_ddl.sql
@@ -24,7 +24,7 @@ create table Person
   emailAddress varchar(80) not null,
   verifyCode int,
   verifyCodeExp int,
-  passwordHash varchar(500),
+  passwordHash varchar(2000),
   phoneNumber varchar(15),
   carrierID int,
   isUser bool,
