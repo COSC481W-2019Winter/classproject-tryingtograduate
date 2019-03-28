6535046202afeb7b259d33502e3b6bab17c326e5
Josh Peck <jpeck3@emich.edu>
2019-03-28 09:25:13 -0400

    restoring Database.php

--- WebPage/PHP/Database.php
+++ WebPage/PHP/Database.php
@@ -1,6 +1,6 @@
 <?php
   const SERVERNAME = "localhost";
-  const USERNAME = "root";
-  const PASSWORD = "pwdpwd";
+  const USERNAME = "kbledsoe3";
+  const PASSWORD = "1784793b4a";
   const DBNAME = "Wi2017_436_kbledsoe3";
 ?>
