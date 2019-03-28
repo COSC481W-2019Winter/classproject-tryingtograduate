3e42d46c958f196c601321377317391f1014a054
Josh Peck <jpeck3@emich.edu>
2019-03-28 09:47:17 -0400

    created Carrier DML

---
+++ Database/mariadb/Carrier_dml.sql
@@ -0,0 +1,7 @@
+INSERT INTO `Carrier`
+  VALUES
+    (1,'Verizon','@vzw.com'),
+    (2,'Sprint','@messaging.sprintpcs.com'),
+    (3,'T-mobile','@tmomail.net'),(4,'AT&T','@txt.att.net'),
+    (5,'Cricket','@mms.cricketwireless.net')
+;
