e9034df11a31e98a19fa2a3150c76b1c377acf6a
hankerdm <46505876+hankerdm@users.noreply.github.com>
2019-03-28 16:57:26 -0400

    resolved bad account addition to db, combined bad password statements

--- WebPage/PHP/Validation.php
+++ WebPage/PHP/Validation.php
@@ -8,7 +8,11 @@ function confirmPassword($password, $passwordCnf)
 		echo 'alert("Password confirmation does not match.")';
 		echo '</script>';
 		returnToHomepage();
+		return false;
 	}
+	
+	//if password and confirmation match return true
+	return true;
 }
 
 function passwordValidation($password) 
@@ -18,36 +22,19 @@ function passwordValidation($password)
 	$uppercase = preg_match('@[A-Z]@', $password);
 	$number = preg_match('@[0-9]@', $password);
 	
-	//if bad password display appropriate message
-	if (!(strlen($password)>7))
-	{
-		echo '<script language="javascript">';
-		echo 'alert("Invalid password. Password must have at least 8 characters.")';
-		echo '</script>';
-	}
-	elseif(!$uppercase)
-	{
-		echo '<script language="javascript">';
-		echo 'alert("Invalid password. Password must have at least 1 capital letter.")';
-		echo '</script>';
-	}
-	elseif(!$number)
-	{
-		echo '<script language="javascript">';
-		echo 'alert("Invalid password. Password must have least 1 number.")';
-		echo '</script>';
-	}
-	elseif(!$lowercase)
-	{
-		echo '<script language="javascript">';
-		echo 'alert("Invalid password. Password must have at least 1 lowercase letter.")';
-		echo '</script>';
-	}
-	
+	//if bad password display appropriate message	
 	if (!(strlen($password)>7) || !$uppercase || !$number || !$lowercase)
 	{
+		echo '<script language="javascript">';
+		echo 'alert("Invalid password. Password must have at least 8 characters and contain the following: " 
+			  + "1 lowercase letter, 1 capital letter, 1 number.")';
+		echo '</script>';
 		returnToHomepage();
+		return false;
 	}
+	
+	//if good password return true
+	return true;
 }
 
 //rerout the user from this php file back to homepage to try again
--- WebPage/PHP/signUp.php
+++ WebPage/PHP/signUp.php
@@ -10,6 +10,7 @@
 	
 	include ('../PHP/Database.php');
 	include ('../PHP/Validation.php');
+	
 	// Create connection
 	$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
 	
@@ -19,25 +20,22 @@
 			echo "could not establish connection to db2...";
 			die("Connection failed: " . $conn->connect_error);	
 		}
+	
+	//check if password is acceptable.
+	if (confirmPassword($password, $passwordCnf) && passwordValidation($password))
+	{
+		//Sets a variable ($query) equal to the mysql query we run to test for existing email in Person table
+		$query = "SELECT emailAddress FROM Person WHERE emailAddress = '$eMail'";
 		
-	//call password validation
-	passwordValidation($password);
-	
-	//call to confirm password
-	confirmPassword($password, $passwordCnf);
-	
-	//Sets a variable ($query) equal to the mysql query we run to test for existing email in Person table
-	$query = "SELECT emailAddress FROM Person WHERE emailAddress = '$eMail'";
-	
-	//Sets a variable ($query2) equal to the mysql query we run to add a user to the Person table
-	$query2 ="INSERT INTO Person(firstName, lastName, emailAddress, passwordHash, phoneNumber) 
-				VALUES ('$fName', '$lName', '$eMail', '$hash', '$phoneNum')";
-				
-	//runs the query and stores the result in a variable called $result
-	$result = $conn->query("$query");
-	
-	//tests the result to see if the query yielded any rows from Person table that match the email entered
-	if ($result->num_rows != 0) 
+		//Sets a variable ($query2) equal to the mysql query we run to add a user to the Person table
+		$query2 ="INSERT INTO Person(firstName, lastName, emailAddress, passwordHash, phoneNumber) 
+					VALUES ('$fName', '$lName', '$eMail', '$hash', '$phoneNum')";
+					
+		//runs the query and stores the result in a variable called $result
+		$result = $conn->query("$query");
+		
+		//tests the result to see if the query yielded any rows from Person table that match the email entered
+		if ($result->num_rows != 0) 
 		{
 			//if email already exists in Person table, user is alerted
 			echo '<script language="javascript">';
@@ -45,35 +43,33 @@
 			echo '</script>';
 			
 			//rerout the user from this php file back to homepage to try again
-			echo '<script language="javascript">';
-			echo 'window.location.href ="../home"' ;
-			echo '</script>';
+			returnToHomepage();
 		}
-	else
-		//if no matching email exists, run second query
+		else  //if no matching email exists, run second query
 		{
 			//runs the second query and stores the result in $result2
 			$result2 = $conn->query("$query2");
+			
 			//checks to see if the user was actually added to the Person table
 			if (mysqli_affected_rows($conn) > 0)
-				{
-					//alerts user of successful registration 
-					echo '<script language="javascript">';
-					echo 'alert("You have registered successfully!!")';
-					echo '</script>';
-					
-					//rerouts user to homepage to sign in with new credentials
-					echo '<script language="javascript">';
-					echo 'window.location.href ="../home"' ;
-					echo '</script>';
-				}
+			{
+				//alerts user of successful registration 
+				echo '<script language="javascript">';
+				echo 'alert("You have registered successfully!!")';
+				echo '</script>';
+				
+				//rerouts user to homepage to sign in with new credentials
+				returnToHomepage();
+			}
 			else
-				{
-					echo '<script language="javascript">';
-					echo 'alert("Something went wrong!!")';
-					echo '</script>';
-				}	
+			{
+				echo '<script language="javascript">';
+				echo 'alert("Something went wrong!!")';
+				echo '</script>';
+			}	
 		}
+	}
+	
 	//Close connection
 	$conn->close();
 ?>
