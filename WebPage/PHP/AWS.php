41e422cb40ed5db0b9198cb108aa22a195d6b4eb
hankerdm <46505876+hankerdm@users.noreply.github.com>
2019-03-30 13:15:42 -0400

    Update AWS.php

--- WebPage/PHP/AWS.php
+++ WebPage/PHP/AWS.php
@@ -2,5 +2,5 @@
   const SERVERNAME = "wi2017436kbledsoe3.cpel0zqeo1dm.us-east-1.rds.amazonaws.com";
   const USERNAME = "kbledsoe3";
   const PASSWORD = "1784793b4a";
-  const DBNAME = "Wi2017436kbledsoe3";
+  const DBNAME = "Wi2017_436_kbledsoe3";
 ?>
