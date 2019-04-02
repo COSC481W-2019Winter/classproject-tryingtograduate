776266cdb6196ae8439d481b0e96d2c45dbb0d5f
Josh Peck <jpeck3@emich.edu>
2019-04-01 20:42:02 -0400

    uninstall script supporting init + carrierpigeon init script cleanup

--- uninstall_service.sh
+++ uninstall_service.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-sudo systemctl stop cpigeon
-sudo systemctl disable cpigeon
-sudo rm -f /etc/systemd/system/cpigeon.service
-sudo rm -f /lib/systemd/system/cpigeon.service
-sudo systemctl daemon-reload
+SYSTEMD=`which systemctl| grep -c systemctl`
+
+if [[ SYSTEMD ]]
+then
+  sudo systemctl stop cpigeon
+  sudo systemctl disable cpigeon
+  sudo rm -f /etc/systemd/system/cpigeon.service
+  sudo rm -f /lib/systemd/system/cpigeon.service
+  sudo systemctl daemon-reload
+else
+  sudo /etc/init.d/carrierpigeon stop
+  sudo chkconfig --del carrierpigeon
+  sudo rm -f /etc/init.d/carrierpigeon
 sudo rm -rf /opt/CarrierPigeon
