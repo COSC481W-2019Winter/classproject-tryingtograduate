d479dd6bd83eadb33e9026060021e9661262f205
Josh Peck <jpeck3@emich.edu>
2019-04-03 20:42:32 -0400

    bugfixes and error logging

--- MessageQueue/MessageQueue.php
+++ MessageQueue/MessageQueue.php
@@ -130,7 +130,7 @@
     $rawMessage = mysqli_fetch_array($messageResult);
 
     $messageId = $rawMessage['messageId'];
-    $ownerId = $rawMessage['uniqueId'];
+    $ownerId = (int)$rawMessage['uniqueId'];
     $firstName = $rawMessage['firstName'];
     $lastName = $rawMessage['lastName'];
     $emailAddress = $rawMessage['emailAddress'];
