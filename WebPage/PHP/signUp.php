54f03e08b96cdecc2a72c2ca8475a0594c8ddffa
kbledsoe3 <kbledsoe3@emich.edu>
2019-04-07 15:28:41 -0400

    Update signUp.php

--- WebPage/PHP/signUp.php
+++ WebPage/PHP/signUp.php
@@ -7,41 +7,41 @@
 	$passwordCnf = filter_input(INPUT_POST, 'passwordNewCnf');
 	$phoneNum = filter_input(INPUT_POST, 'phone');
 	$hash = password_hash($password, PASSWORD_DEFAULT);
-	
+
 	include ('../PHP/Database.php');
 	include ('../PHP/Validation.php');
-	
+
 	// Create connection
 	$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
-	
+
 	// Check connection
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 		{
 			echo "could not establish connection to db2...";
-			die("Connection failed: " . $conn->connect_error);	
+			die("Connection failed: " . $conn->connect_error);
 		}
-	
+
 	//check if password is acceptable.
 	if (confirmPassword($password, $passwordCnf) && passwordValidation($password))
 	{
 		//Sets a variable ($query) equal to the mysql query we run to test for existing email in Person table
 		$query = "SELECT emailAddress FROM Person WHERE emailAddress = '$eMail'";
-		
+
 		//Sets a variable ($query2) equal to the mysql query we run to add a user to the Person table
-		$query2 ="INSERT INTO Person(firstName, lastName, emailAddress, passwordHash, phoneNumber) 
+		$query2 ="INSERT INTO Person(firstName, lastName, emailAddress, passwordHash, phoneNumber)
 					VALUES ('$fName', '$lName', '$eMail', '$hash', '$phoneNum')";
-					
+
 		//runs the query and stores the result in a variable called $result
 		$result = $conn->query("$query");
-		
+
 		//tests the result to see if the query yielded any rows from Person table that match the email entered
-		if ($result->num_rows != 0) 
+		if ($result->num_rows != 0)
 		{
 			//if email already exists in Person table, user is alerted
 			echo '<script language="javascript">';
 			echo 'alert("Email already registered.")';
 			echo '</script>';
-			
+
 			//rerout the user from this php file back to homepage to try again
 			returnToHomepage();
 		}
@@ -49,15 +49,52 @@
 		{
 			//runs the second query and stores the result in $result2
 			$result2 = $conn->query("$query2");
-			
+
 			//checks to see if the user was actually added to the Person table
 			if (mysqli_affected_rows($conn) > 0)
 			{
-				//alerts user of successful registration 
+				//if successful, get ownerId from Person table
+				$query3 = "SELECT ownerId FROM Person WHERE emailAddress = ''$email'";
+				$result3 = $conn->query("$query3");
+				$object = mysqli_fetch_object($result3);
+				$ownerId = $object->ownerId;
+
+				//create predefined messages
+				$subject1 = "Weather Alert";
+				$content1 = "Due to inclement conditions, some schedules may have changed.  Please check with your supervisor to confirm your shift.";
+				$template1 = "Weather Alert";
+				$subject2 = "Office Closure";
+				$content2 = "The office will be closed today, Month Day Year.";
+				$template2 = "Office Closure";
+				$subject3 = "Staff Meeting";
+				$content3 = "There will be a staff meeting at the end of shift today.  Attendance is required.";
+				$template3 = "Staff Meeting";
+				$subject4 = "Employee of the Month";
+				$content4 = "This month's winner of the Employee of the Month is, firstName lastName!!";
+				$template4 = "EOM Winner";
+
+				//add predefined message to user's templates
+				$query4 ="INSERT INTO Message(ownerId, subject, content, templateName)
+							VALUES ('$ownerId', '$subject1', '$content1', '$template1')";
+				$result4 = $conn->query("$query4");
+
+				$query5 ="INSERT INTO Message(ownerId, subject, content, templateName)
+							VALUES ('$ownerId', '$subject2', '$content2', '$template2')";
+				$result5 = $conn->query("$query5");
+
+				$query6 ="INSERT INTO Message(ownerId, subject, content, templateName)
+							VALUES ('$ownerId', '$subject3', '$content3', '$template3')";
+				$result6 = $conn->query("$query6");
+
+				$query7 ="INSERT INTO Message(ownerId, subject, content, templateName)
+							VALUES ('$ownerId', '$subject4', '$content4', '$template4')";
+				$result7 = $conn->query("$query7");
+
+				//alerts user of successful registration
 				echo '<script language="javascript">';
 				echo 'alert("You have registered successfully!!")';
 				echo '</script>';
-				
+
 				//rerouts user to homepage to sign in with new credentials
 				returnToHomepage();
 			}
@@ -66,11 +103,10 @@
 				echo '<script language="javascript">';
 				echo 'alert("Something went wrong!!")';
 				echo '</script>';
-			}	
+			}
 		}
 	}
-	
+
 	//Close connection
 	$conn->close();
 ?>
-
