22b8214b0e51f6cd03877fdd91580f56dd85506b
Josh Peck <jpeck3@emich.edu>
2019-04-08 00:00:55 -0400

    syntax error fix in QueueConfig.php

--- MessageQueue/QueueConfig.php
+++ MessageQueue/QueueConfig.php
@@ -2,5 +2,5 @@
   const SLEEP_INTERVAL = 30;
   const SERVICE_EMAIL = "jpeck3@emich.edu";
   const SERVICE_NAME = "Carrier Pigeon";
-  const SERVICE_SUBJECT = "Carrier Pigeon Message Report";S
+  const SERVICE_SUBJECT = "Carrier Pigeon Message Report";
 ?>
