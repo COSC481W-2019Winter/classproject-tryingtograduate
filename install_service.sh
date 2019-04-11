c408f0fbf023f605509d25f66d1a4cfb8cf186e5
Josh Peck <jpek3@emich.edu>
2019-04-11 02:19:58 +0000

    simplified script for aws/rh6- init.d service installation seperate systemctl script located in Util

--- install_service.sh
+++ install_service.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-SYSTEMD=`which systemctl| grep -c systemctl`
 USEREXISTS=`grep -c cpigeon /etc/passwd`
 
 sudo mkdir /opt/CarrierPigeon
@@ -9,18 +8,7 @@ if [[ USEREXISTS == 0 ]]
 then
   sudo adduser cpigeon
 fi
-
-if [[ $SYSTEMD == 1 ]]
-then
-  sudo cp Util/cpigeon.service /lib/systemd/system/
-  sudo ln -s /lib/systemd/system/cpigeon.service /etc/systemd/system/cpigeon.service
-  sudo systemctl daemon-reload 
-  sudo systemctl enable cpigeon
-  sudo systemctl start cpigeon
-  sudo systemctl status cpigeon
-else
-  sudo mv /opt/CarrierPigeon/MessageQueue/AWS.php /opt/CarrierPigeon/MessageQueue/Database.php
-  sudo cp Util/carrierpigeon /etc/init.d/
-  sudo chkconfig --add carrierpigeon
-  sudo /etc/init.d/carrierpigeon start 
-fi
+sudo mv /opt/CarrierPigeon/MessageQueue/AWS.php /opt/CarrierPigeon/MessageQueue/Database.php
+sudo cp Util/carrierpigeon /etc/init.d/
+sudo chkconfig --add carrierpigeon
+sudo /etc/init.d/carrierpigeon start 
--- uninstall_service.sh
+++ uninstall_service.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-SYSTEMD=`which systemctl| grep -c systemctl`
-
-if [[ SYSTEMD == 1 ]]
-then
-  sudo systemctl stop cpigeon
-  sudo systemctl disable cpigeon
-  sudo rm -f /etc/systemd/system/cpigeon.service
-  sudo rm -f /lib/systemd/system/cpigeon.service
-  sudo systemctl daemon-reload
-else
-  sudo /etc/init.d/carrierpigeon stop
-  sudo chkconfig --del carrierpigeon
-  sudo rm -f /etc/init.d/carrierpigeon
-fi
+sudo /etc/init.d/carrierpigeon stop
+sudo chkconfig --del carrierpigeon
+sudo rm -f /etc/init.d/carrierpigeon
 sudo rm -rf /opt/CarrierPigeon
