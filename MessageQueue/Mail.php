399597294d83df01fa571fb9fe52cd831c842755
Josh Peck <jpeck3@emich.edu>
2019-04-11 23:53:48 -0400

    troubleshooting sms login

--- MessageQueue/Mail.php
+++ MessageQueue/Mail.php
@@ -75,7 +75,7 @@ class Mail
           array_push($results, $this->reportSuccess($contactName, $emailAddress));
         }
       }
-      if($carrier != null)
+      if($phoneNumber != null)
       {
         $sms = preg_replace('/[^0-9]/', '', $phoneNumber);
         $sms .= $carrier->getEmail();
