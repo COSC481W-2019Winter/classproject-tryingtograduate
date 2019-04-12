f2d5c3830c9873cc330d360d2ae3d642966b1af3
kbledsoe3 <kbledsoe3@emich.edu>
2019-04-12 17:28:54 -0400

    Update signIn.php

--- WebPage/PHP/signIn.php
+++ WebPage/PHP/signIn.php
@@ -4,25 +4,20 @@
 	$username = filter_input(INPUT_POST, 'usernameEmail');
 	$passWordEst = filter_input(INPUT_POST, 'passwordEst');
 	$_SESSION['currentUserEmail'] = $username;
-
 	//include files
 	include ('../PHP/Database.php');
 	include ('../PHP/Validation.php');
-
 	// Create connection
 	$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
-
 	// Check connection
 	if ($conn->connect_error)
 	{
 		echo "could not establish connection to db2...";
 		die("Connection failed: " . $conn->connect_error);
 	}
-
 	//Sets aand run query then store in result
-	$query = "SELECT emailAddress, passwordHash FROM Person WHERE emailAddress = '$username' AND passwordHash = '$passwordEst' AND ownerId IS NULL";
+	$query = "SELECT emailAddress FROM Person WHERE emailAddress = '$username' AND ownerId IS NULL";
 	$result = $conn->query("$query");
-
 	//tests the result to see if the query yielded any rows from our Person table
 	if ($result->num_rows != 0)
 	{
@@ -31,7 +26,6 @@
 		$resultHash = $conn->query($queryHash);
 		$resultHashRow = $resultHash->fetch_assoc();
 		$hash = $resultHashRow['passwordHash'];
-		
 		if (password_verify($passWordEst, $hash))
 		{
 			//routs the user to the Message Dashboard if username and password were found in same row of table
@@ -42,6 +36,9 @@
 		else
 		{
 			//password not found, alert and return to home
+			echo '<script language="javascript">';
+			echo 'window.location.href ="../dashboard"' ;
+			echo '</script>';
 			invlidUserOrPass();
 		}
 	}
@@ -50,7 +47,6 @@
 		//username not found, alert and return to home.
 		invlidUserOrPass();
 	}
-
 	//Close connection
 	$conn->close();
 ?>
