816759c19621c3b6a639ee8d2b3c0c2afc1417e0
hankerdm <46505876+hankerdm@users.noreply.github.com>
2019-04-18 21:27:39 -0400

    added reges to verification page

--- WebPage/Verification/index.php
+++ WebPage/Verification/index.php
@@ -20,6 +20,7 @@
 	</head>
 	<body>
 		<h1 id = "Company" style = "text-align: center" > User Verification </h1>
+		<p id="messageAlert" style = "text-align: center"></p>
 		<div class = "center" style = "text-align: center">
 			<form name="verify" action = "" method = "post">
 			<b>A verification code has been sent to the email you provided.</b></br>  
@@ -42,29 +43,45 @@
 		$UserEmail = $_POST['email'];
 		$code = $_POST['code'];
 		
-		//query to check if code entered matches the verifyCode in same row as uniqueId of current user
-		$result1 = mysqli_query($conn, "SELECT uniqueId from Person WHERE verifyCode = '$code' AND emailAddress = '$UserEmail'");
-		//if query results in a row found
-		if ($result1->num_rows != 0) 
+		//Regex for user inputs
+		$emailRegex = "/^[.0-9A-Z_a-z]+@[.0-9A-Z_a-z]+.[a-zA-Z]{3}$/";
+		$codeRegex = "/^[0-9]+$/";
+		
+		//check for valid input
+		if (!preg_match($emailRegex, $UserEmail))
 		{
-			//run query to update Person table with NULL values for verifyCode and ownerId for current user
-			$query2 = "UPDATE Person SET ownerId = NULL, verifyCode = NULL WHERE verifyCode = '$code' AND emailAddress = '$UserEmail'";
-			$result2 = $conn->query($query2);
-			echo '<script language="javascript">';
-			echo 'alert("Email Verified Successfully!")';
-			echo '</script>';
-			echo '<script language="javascript">';
-			echo 'window.location.href ="../home/index.html"';
-			echo '</script>';
+			echo '<script language="javascript"> document.getElementById("messageAlert").innerHTML = "Invalid email" </script>';
 		}
-		else
+		else if (!preg_match($codeRegex, $code))
 		{
-			echo '<script language="javascript">';
-			echo 'alert("Did not find email and code match. Please try again.")';
-			echo '</script>';
-			echo '<script language="javascript">';
-			echo 'window.location.href ="../Verification/index.php"';
-			echo '</script>';
+			echo '<script language="javascript"> document.getElementById("messageAlert").innerHTML = "Invalid code entry" </script>';
+		}
+		else //if good input check for verification
+		{
+			//query to check if code entered matches the verifyCode in same row as uniqueId of current user
+			$result1 = mysqli_query($conn, "SELECT uniqueId from Person WHERE verifyCode = '$code' AND emailAddress = '$UserEmail'");
+			//if query results in a row found
+			if ($result1->num_rows != 0) 
+			{
+				//run query to update Person table with NULL values for verifyCode and ownerId for current user
+				$query2 = "UPDATE Person SET ownerId = NULL, verifyCode = NULL WHERE verifyCode = '$code' AND emailAddress = '$UserEmail'";
+				$result2 = $conn->query($query2);
+				echo '<script language="javascript">';
+				echo 'alert("Email Verified Successfully!")';
+				echo '</script>';
+				echo '<script language="javascript">';
+				echo 'window.location.href ="../home/index.html"';
+				echo '</script>';
+			}
+			else
+			{
+				echo '<script language="javascript">';
+				echo 'alert("Did not find email and code match. Please try again.")';
+				echo '</script>';
+				echo '<script language="javascript">';
+				echo 'window.location.href ="../Verification/index.php"';
+				echo '</script>';
+			}
 		}
 	}
 //Close connection
